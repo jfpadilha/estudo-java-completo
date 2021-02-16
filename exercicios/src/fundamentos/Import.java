d433f479e19c2c1307bda763d8d7acce1c7a7de5
Joao F P S <jfpadilha@gmail.com>
2021-02-16 14:16:33 -0300

    #24 Imports

---
+++ exercicios/src/fundamentos/Import.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+import java.util.Date;
+
+public class Import {
+	
+	public static void main(String[] args) {
+		java.lang.String n = "Olá mundo"; //o que esta em java.lan não é necessario import
+		System.out.println(n);
+		
+		String s = "Helo world";
+		System.out.println(s);
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+//		JButton botao = new JButton();
+	}
+}
