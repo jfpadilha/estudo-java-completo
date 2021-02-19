ca53b9c937be138ab0a12c8231becd2260512ddd
Joao F P S <jfpadilha@gmail.com>
2021-02-18 23:21:54 -0300

    #27 - Objeto vs Primitivo - separando o que é objeto do que é wrapper

---
+++ exercicios/src/fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto {
+	public static void main(String[] args) {
+		
+		String s = new String("texto"); //"s" é um objeto
+		s.toUpperCase();	//o objeto tem atributos e comportamentos
+
+//		Tudo em java eh objeto, menos os 8 tipos primitivos
+		
+//		WRAPPERS: sao a versao objeto dos tipos primitivos
+		int a = 123;	
+//		"a." não tem nenhum comportamentopois nao eh objeto
+
+		
+		
+	}
+}
