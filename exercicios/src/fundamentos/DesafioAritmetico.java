1cc93ed2bbdb85e11ff053c435984e79c68f6899
Joao F P S <jfpadilha@gmail.com>
2021-02-23 22:46:29 -0300

    #38 & #39 - Desafio Operadores aritmeticos

---
+++ exercicios/src/fundamentos/DesafioAritmetico.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+public class DesafioAritmetico {
+	public static void main(String[] args) {
+
+		int a = 3 * 4 - 10;
+		int cubo = (int) Math.pow(a, 3); // "pow" de potencia "a elevado a 3"
+		double cubod = Math.pow(a, 3); // "pow" de potencia "a elevado a 3"
+
+		System.out.println(cubo);
+		System.out.println(cubod);
+
+		System.out.println("--------------------");
+
+		int a1 = (int) Math.pow((6 * (3 + 2)), 2) / (3 * 2);
+
+		int b1 = (int) (Math.pow((1 - 5) * (2 - 7) / 2, 2));
+
+		int c1 = (int) (Math.pow((a1 - b1), 3)) / (int) Math.pow(10, 3);
+
+		System.out.println(c1);
+
+	}
+
+}
