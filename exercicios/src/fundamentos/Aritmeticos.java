7974a0f70a7f49fadb753b380d8f4ea0e1537d8f
Joao F P S <jfpadilha@gmail.com>
2021-02-23 21:57:55 -0300

    #37 - Operadores aritmeticos

---
+++ exercicios/src/fundamentos/Aritmeticos.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class Aritmeticos {
+	public static void main(String[] args) {
+//		Aritimeticos:
+		int i = 2 + 3;
+		var x = 34.567;
+		double y = 2.2;
+
+		int a = 8;
+		int b = 3;
+
+		System.out.println(x + y);
+		System.out.println(x - y);
+		System.out.println(x * y);
+		System.out.println(x / (double) y);
+		System.out.println(a / (double) b);
+	 	System.out.println(8 % (double) 3); // Modulo, eh o resto da divisao
+	}
+}
