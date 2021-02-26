e2fcb164410844eb789d1fb1e7c150a3c9c3c950
Joao F P S <jfpadilha@gmail.com>
2021-02-25 21:01:25 -0300

    #40 Operadores lógicos #01 (AND, OR, XOR e NOT)

--- exercicios/src/fundamentos/operadores/Logicos.java
+++ exercicios/src/fundamentos/operadores/Logicos.java
@@ -1,5 +1,15 @@
 package fundamentos.operadores;
 
 public class Logicos {
+	public static void main(String[] args) {
+		boolean cond1 = true;
+		boolean cond2 = 3 > 7;
 
+		System.out.println(cond1 && cond2);
+		System.out.println(cond1 || cond2);
+		System.out.println(cond1 ^ cond2); // OR exclusivo
+		System.out.println(cond1 && cond2);
+		System.out.println(cond1 && cond2);
+
+	}
 }
