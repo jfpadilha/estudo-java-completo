2a0c4d80efb220dd4d82fef04c972f8d61440d1d
Joao F P S <jfpadilha@gmail.com>
2021-02-26 23:16:09 -0300

    #47 Operador ternario - var = cond1 ? 'result1' : 'result 2'

---
+++ exercicios/src/fundamentos/operadores/Ternario.java
@@ -0,0 +1,18 @@
+package fundamentos.operadores;
+
+public class Ternario {
+	public static void main(String[] args) {
+		
+		double media = 5;
+		String resultado = media >= 7.0 ? "Aprovado" : "Reprovado";
+//		VARIAVEL = MEDIA "se for maior ou igual a 7" PRIMEIRA CONDICAO ":" SEGUNDA CONDICAO
+		
+		String result = media >= 7.0 ? "aprovado" : media >= 5 ? "em recuperação" : "reprovado";
+//		VARIAVEL = MEDIA "se for maior ou igual a 7" PRIMEIRA CONDICAO ":" SEGUNDA CONDICAO
+		
+		System.out.println(resultado);
+		System.out.println(result);
+		
+	}
+
+}
