74fc3c27e4452649e2c9942f6af39fb2298bf73a
Joao F P S <jfpadilha@gmail.com>
2021-02-28 09:29:30 -0300

    #64 - While 01 (determinado)

---
+++ exercicios/src/controle/WhileDeterminado.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class WhileDeterminado {
+	public static void main(String[] args) {
+		int cont = 1;
+
+		while (cont <= 30) {
+			System.out.println("Hello world: " + cont);
+			cont +=2;
+		}
+
+		
+	}
+}
