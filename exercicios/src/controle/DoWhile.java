2cc2132d2975edf2d70a32b2c2aec5f63fbaf826
Joao F P S <jfpadilha@gmail.com>
2021-02-28 11:41:59 -0300

    #67 - Do/While - entra pelo menos 1 vez no laço

--- exercicios/src/controle/DoWhile.java
+++ exercicios/src/controle/DoWhile.java
@@ -3,30 +3,23 @@ package controle;
 import java.text.Normalizer;
 import java.util.Scanner;
 
-/**
- * Ignorar o texto de entrada
- * 
- * Valiando registrando texto sem acentos
- * 
- * Removendo acentos e maiusculas
- * 
- * @author joao
- *
- */
+public class DoWhile {
 
-public class WhileIndeterminado {
 	public static void main(String[] args) {
-		Scanner e = new Scanner(System.in);
 
+		Scanner e = new Scanner(System.in);
 		String s = "";
 
-		while (!s.contains("sair")) {
+		do {
 			System.out.println("sair?");
+
 			s = e.nextLine().toLowerCase();
 			s = Normalizer.normalize(s, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
-		}
 
-		System.out.println("Saiu!");
+		} while (!s.contains("sair"));
+
+		System.out.println("saiu!");
+
 		e.close();
 	}
 }
