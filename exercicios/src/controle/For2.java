e82a6320c6c763f26e90047771f22732c3adf7b4
Joao F P S <jfpadilha@gmail.com>
2021-03-01 22:45:30 -0300

    #70 - For #02

---
+++ exercicios/src/controle/For2.java
@@ -0,0 +1,19 @@
+package controle;
+
+/**
+ * Criuar um laço for decrescente de 10 a 0 de 2 em 2
+ * 
+ * @author joao
+ *
+ */
+
+public class For2 {
+	public static void main(String[] args) {
+
+		for (int i = 10; i >= 0; i -= 2) {
+			System.out.println(i);
+		}
+
+	}
+
+}
