7ab1dfefcb097a0a9aabf9c4e7d4a683bf55c42b
Joao F P S <jfpadilha@gmail.com>
2021-03-01 23:06:00 -0300

    #71 - For #03

---
+++ exercicios/src/controle/For3.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class For3 {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+
+		for (i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.printf("[%d %d]\n", i, j);
+			}
+		}
+	}
+}
