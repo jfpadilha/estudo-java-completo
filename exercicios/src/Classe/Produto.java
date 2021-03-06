a2c17fdff24438f880d74f7a1526c94721924ef3
Joao F P S <jfpadilha@gmail.com>
2021-03-06 13:29:11 -0300

    #90 & #91 - Primeiros métodos classe produto

--- exercicios/src/Classe/Produto.java
+++ exercicios/src/Classe/Produto.java
@@ -6,5 +6,16 @@ public class Produto {
 	String nome;
 	double preco;
 	double desconto;
- 
+
+	// retorno eh double
+	double precoComDesconto() {
+		return preco - (preco * desconto / 100);
+//		return preco * (1 - desconto);
+	}
+
+	// retorno double, mesmo nome, mas a assinatura eh diferente
+	double precoComDesconto(double descontoDoGerente) {
+		return preco - (preco * (desconto + descontoDoGerente) / 100);
+//		return preco * (1 - desconto);
+	}
 }
--- exercicios/src/Classe/ProdutoTeste.java
+++ exercicios/src/Classe/ProdutoTeste.java
@@ -14,14 +14,14 @@ public class ProdutoTeste {
 		Produto p1 = new Produto();
 		p1.nome = "Notebook";
 		p1.preco = 2000.00;
-		p1.desconto = 0.25;
+		p1.desconto = 25;
 
 		var p2 = new Produto();
 		p2.nome = "Caneta preta";
 		p2.preco = 12.56;
-		p2.desconto = 0.29;
+		p2.desconto = 25;
 
-		System.out.println(p2.nome);
-		System.out.println(p1.preco * (1 - p1.desconto));
+		System.out.printf("p1: %.2f\n", p1.precoComDesconto(25));
+		System.out.printf("p2: %.2f\n", p2.precoComDesconto());
 	}
 }
