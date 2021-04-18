6a1ea5f814bc9ba1f4cec9acf5dc3e720ffa6bc9
Joao F P Souza <jfpadilha@gmail.com>
2021-04-18 02:38:18 -0300

    #109 - Valores Null

--- exercicios/src/classe/valorNulo.java
+++ exercicios/src/classe/valorNulo.java
@@ -5,10 +5,12 @@ package classe;
  * 
  * Variaveis nao inicializadas gera erros de compilacao
  * 
- * Variavel inicializada como "null" geram erros de runtime (em tempo de
- * execucao) "null pointer exception" que eh uma variavel nula, tentar acessar
- * algo dela, soh vai acontecer no momento da execucao, o eclipse nao vai battar
- * o e
+ * Erro de runtime, erro em tempo de execução, ex.: < String s2 = null; > a
+ * variável foi inicializada, o código irá compilar, porém ocorrerá o erro
+ * "NullPointerException" é a tentativa de acessar algum atributo ou
+ * comportamento de uma variável nula não se pode nem acessar um método ou
+ * atributo de algo que está nulo ou algo não aponta para um objeto real na
+ * memória
  *
  * 
  * 
@@ -19,11 +21,21 @@ package classe;
 public class valorNulo {
 
 	public static void main(String[] args) {
-		String s1 = "";
-		System.out.println(s1.concat("???"));
 
-		String s2 = null; // gera erro "null pointer exception" erro ao apontar o local na memoria
-		System.out.println(s2.concat("???"));
+		String s1 = "";
+		System.out.println(s1.concat("!!!"));
+
+		Data dt = Math.random() > 0.5 ? new Data() : null;
+
+		if (dt != null) {
+			dt.mes = 3;
+			System.out.println(dt.obterDataFormatada());
+		}
+
+		String s2 = Math.random() > 0.5 ? s2 = "Ok" : null;
+		if (s2 != null) {
+			System.out.println(s2.concat(" -> não é nulo"));
+		}
 
 	}
 
